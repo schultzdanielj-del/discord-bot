1e7896f225256f51ac3439b5095396165e1bd42c
schultzdanielj-del <schultzdanielj@gmail.com>
2026-02-08 21:58:58 -0500

    Update PRBot.py

--- PRBot.py
+++ PRBot.py
@@ -362,7 +362,7 @@ async def store_pr(user_id, username, exercise, weight, reps, estimated_1rm, mes
             response = await client.post(
                 f"{API_BASE_URL}/prs",
                 json={
-                    "user_id": user_id,
+                    "user_id": int(user_id),
                     "exercise": exercise,
                     "weight": weight,
                     "reps": reps
